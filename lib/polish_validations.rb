7b9ad40d4ed40abc0f358b68387471daee1b6155
jablkopp <jablko@gmail.com>
2011-04-11 17:16:15 +0200

    active_record fix

--- lib/polish_validations.rb
+++ lib/polish_validations.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-require 'activerecord'
+require 'active_record'
 
 module PolishValidations
   def validates_as_bank_account(*attr_names)
@@ -7,9 +7,9 @@ module PolishValidations
     configuration.update(attr_names.extract_options!)
 
     validates_each(attr_names, configuration) do |record, attr_name, value|
-      value.gsub!(/[^0-9]/,'')
+      value.to_s.gsub!(/[^0-9]/,'')
       z = 0
-      if value.length == 26
+      if value.to_s.length == 26
         n = value + "2521"
         w = [1,10,3,30,9,90,27,76,81,34,49,5,50,15,53,45,62,38,89,17,73,51,25,56,75,71,31,19,93,57]
         x = n[2..-1] + n[0,2]
@@ -25,7 +25,7 @@ module PolishValidations
 
     validates_each(attr_names, configuration) do |record, attr_name, value|
       value.to_s.gsub!(/[^0-9]/,'')
-      nip_digits = value.split('').map(&:to_i)
+      nip_digits = value.to_s.split('').map(&:to_i)
       unless value.present? && nip_digits.length == 10 &&
           (0..8).inject(0) {|s,i| s += nip_digits[i] * [6,5,7,2,3,4,5,6,7][i] } % 11 == nip_digits.last
         record.errors.add attr_name, :invalid, :default => configuration[:message], :value => value
